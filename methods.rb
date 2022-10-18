bce7f3b412bd7c5c1ef6ced596bacc583614a150
Tricky Licky <wangombevictor5@gmail.com>
2022-10-18 10:48:04 +0300

    complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (a,b)
+    a+b
+end
+
+def halve int
+    if int.class == Integer
+        return int/2
+    end
+    nil
+end
